91331ee7cddeacca8da62dcd969e904e24d3a551
Casseiopeia <27986852+Cassiopeia19@users.noreply.github.com>
2019-02-21 15:08:39 -0600

    Add files via upload

---
+++ SpringAssignment1/src/main/java/edu/umsl/SpringAssignment1/Student.java
@@ -0,0 +1,16 @@
+package edu.umsl.SpringAssignment1;
+
+public class Student {
+	
+	public void getName() {
+		
+		System.out.println("Name: Timothi Purcell");
+	}
+	
+	public void getSSN() {
+		
+		System.out.println("SSN: 123456789");
+		
+	}
+
+}
